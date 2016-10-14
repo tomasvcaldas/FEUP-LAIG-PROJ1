367303b899d3cbdc26e3d7458ea855f47018ca36
Ines Ferreira <inesferreira789@gmail.com>
2016-10-14 14:54:06 +0100

    Drawing triangle

--- reader/MyTriangle.js
+++ reader/MyTriangle.js
@@ -8,12 +8,12 @@ function Triangle(scene, x1, y1, z1, x2, y2, z2, x3, y3, z3) {
     this.x1 = x1;
     this.y1 = y1;
     this.z1 = z1;
-    this.x2 = x1;
-    this.y2 = y1;
-    this.z2 = z1;
-    this.x3 = x1;
-    this.y3 = y1;
-    this.z3 = z1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.z2 = z2;
+    this.x3 = x3;
+    this.y3 = y3;
+    this.z3 = z3;
 
     this.initBuffers();
 };
